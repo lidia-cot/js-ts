a70de1d5ec1f227772daf9579ce4ded87ebffc3f
Lidia BitBucket <mslydka@ymail.com>
2020-08-21 09:44:44 +0300

    math operations part

---
+++ client/index.ts
@@ -0,0 +1,84 @@
+import rp from 'request-promise'
+
+const path: string = 'http://localhost:8080';
+const sum_path: string = 'http://localhost:8080/sum';
+const subtract_path: string = 'http://localhost:8080/subtract';
+const divide_path: string = 'http://localhost:8080/divide';
+const multiply_path: string = 'http://localhost:8080/multiply';
+
+var sum = {
+    method: 'POST',
+    uri: path + '/sum',
+    body: {
+        param1: 4,
+        param2: 2
+    },
+    json: true
+};
+
+
+var subtract = {
+    method: 'POST',
+    uri: path + '/subtract',
+    body: {
+        param1: 4,
+        param2: 2
+    },
+    json: true
+};
+
+var multiply = {
+    method: 'POST',
+    uri: path + '/multiply',
+    body: {
+        param1: 4,
+        param2: 2
+    },
+    json: true
+};
+
+var divide = {
+    method: 'POST',
+    uri: path + '/divide',
+    body: {
+        param1: 4,
+        param2: 2
+    },
+    json: true
+};
+
+var trial = {
+    method: 'GET',
+    uri: path,
+    json: true
+};
+
+ async function main() {
+    //var options = [sum, subtract, multiply, divide];
+
+    // options.forEach((element: any) => {
+    //     try {
+    //         let parsedBody = rp(options[element]);
+    //         console.log(parsedBody);
+    //     } catch(err) {
+    //         console.log(err);
+    //     }
+    // })
+
+    // try {
+    //     let parsedBody = await rp(sum);
+    //     console.log(parsedBody);
+    //     } catch(err) {
+    //         console.log(err);
+    //     }
+
+    //rp.post(path, {json:true, body> {param1:"2", param2: "3"}});
+    let promiseArray = [];
+
+    promiseArray.push(rp.post(path, {json:true, body> {param1:"2", param2: "3"}}));
+
+    let result = await Promise.all(promiseArray);
+}
+
+
+main();
---
+++ server/index.ts
@@ -0,0 +1,35 @@
+import express, { request, response } from 'express';
+
+//define server port
+const PORT: number = 8080;
+
+//create server
+const server: express.Application = express();
+
+//configure server
+server.use(express.json());
+server.use(express.urlencoded());
+
+server.get('/', (request: express.Request, response: express.Response) => {
+    response.sendfile('login.html');
+});
+
+server.post('/sum',(request: express.Request, response: express.Response) => {
+    response.send(`Your result after summing is: ${+request.body.param1 + +request.body.param2}`);
+});
+
+server.post('/subtract',(request: express.Request, response: express.Response) => {
+    response.send(`Your result after subtracting is: ${+request.body.param1 - +request.body.param2}`);
+});
+
+server.post('/multiply',(request: express.Request, response: express.Response) => {
+    response.send(`Your result after multiplying is: ${+request.body.param1 * +request.body.param2}`);
+});
+
+server.post('/divide',(request: express.Request, response: express.Response) => {
+    response.send(`Your result after dividing is: ${+request.body.param1 / +request.body.param2}`);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}!`)
+});
